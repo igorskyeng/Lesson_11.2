2723428cb01a60bf2570bd3897e5792773393f3a
Igor Barbur <Igor.barbur@yandex.ru>
2023-12-12 15:16:26 +0800

    add title text in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper_text(value):
-    """"Пеоевод всех букв в верхний регистр""
+    """Пеоевод всех букв в верхний регистр"""
     return value.upper()
+
+
+def title_text(value):
+    """Перевод всех первых букв в верхний регистр"""
+    return value.title()
